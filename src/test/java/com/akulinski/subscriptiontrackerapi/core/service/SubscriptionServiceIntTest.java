15c38792981a9b3ec8c1c024a182e37bae32a1c0
akulinski <albert.kulinski@hotmail.com>
2020-04-01 21:50:54 +0200

    added test for find all subscrpions by user

--- src/test/java/com/akulinski/subscriptiontrackerapi/core/service/SubscriptionServiceIntTest.java
+++ src/test/java/com/akulinski/subscriptiontrackerapi/core/service/SubscriptionServiceIntTest.java
@@ -1,6 +1,8 @@
 package com.akulinski.subscriptiontrackerapi.core.service;
 
 import com.akulinski.subscriptiontrackerapi.PostgresqlContainerExtension;
+import com.akulinski.subscriptiontrackerapi.core.repository.SubscriptionRepository;
+import com.akulinski.subscriptiontrackerapi.core.repository.UserRepository;
 import com.akulinski.subscriptiontrackerapi.core.service.dto.SubscriptionDTO;
 import com.akulinski.subscriptiontrackerapi.core.service.dto.UserDTO;
 import org.junit.Assert;
@@ -12,18 +14,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application.yaml")
 @ExtendWith(PostgresqlContainerExtension.class)
 class SubscriptionServiceIntTest {
 
+  @Autowired private SubscriptionRepository subscriptionRepository;
+
+  @Autowired private UserRepository userRepository;
+
   @Autowired private UserService userService;
 
   @Autowired private SubscriptionService subscriptionService;
 
   @BeforeEach
-  void setUp() {}
+  void setUp() {
+
+    subscriptionRepository.deleteAll();
+
+    userRepository.deleteAll();
+  }
 
   @Test
   void save() {
@@ -31,12 +43,12 @@ class SubscriptionServiceIntTest {
     userDTO.setUsername("user");
     userDTO.setPassword("user");
 
-    final var userDTO1 = userService.create(userDTO);
+    userService.create(userDTO);
 
     SubscriptionDTO subscriptionDTO = new SubscriptionDTO();
     subscriptionDTO.setPoster("user");
 
-    final var subscriptionDTO1 = subscriptionService.save(subscriptionDTO);
+    subscriptionService.save(subscriptionDTO);
 
     final var byPoster = subscriptionService.findByPoster("user");
 
@@ -46,5 +58,32 @@ class SubscriptionServiceIntTest {
             .collect(Collectors.toUnmodifiableList());
 
     Assert.assertEquals(1, subscriptionDTOList.size());
+  }
+
+  @Test
+  void findAll() {
+
+    UserDTO userDTO = new UserDTO();
+    userDTO.setUsername("user");
+    userDTO.setPassword("user");
+    userService.create(userDTO);
+
+    Stream.generate(
+            () -> {
+              SubscriptionDTO subscriptionDTO = new SubscriptionDTO();
+              subscriptionDTO.setPoster("user");
+              return subscriptionDTO;
+            })
+        .limit(10)
+        .forEach(subscriptionService::save);
+
+    final var byPoster = subscriptionService.findByPoster("user");
+
+    final var subscriptionDTOList =
+        byPoster.stream()
+            .filter(subscriptionDTO2 -> "user".equals(subscriptionDTO2.getPoster()))
+            .collect(Collectors.toUnmodifiableList());
+
+    Assert.assertEquals(10, subscriptionDTOList.size());
   }
 }
