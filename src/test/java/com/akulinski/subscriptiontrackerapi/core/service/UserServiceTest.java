610190c86e8476f5b086d90b8d1ccfe70ea126a8
akulinski <albert.kulinski@hotmail.com>
2020-04-02 17:30:38 +0200

    added test for user service

---
+++ src/test/java/com/akulinski/subscriptiontrackerapi/core/service/UserServiceTest.java
@@ -0,0 +1,35 @@
+package com.akulinski.subscriptiontrackerapi.core.service;
+
+import com.akulinski.subscriptiontrackerapi.core.repository.UserRepository;
+import com.akulinski.subscriptiontrackerapi.core.service.dto.UserDTO;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application.yaml")
+class UserServiceTest {
+
+  @Autowired private UserService userService;
+
+  @Autowired private UserRepository userRepository;
+
+  @BeforeEach
+  void setUp() {
+    userRepository.deleteAll();
+  }
+
+  @Test
+  void create() {
+    UserDTO userDTO = new UserDTO();
+    userDTO.setUsername("user");
+    userDTO.setPassword("user");
+    final var userDTO1 = userService.create(userDTO);
+    final var byId = userRepository.findById(userDTO1.getId());
+
+    Assert.assertNotNull(byId.get());
+  }
+}
